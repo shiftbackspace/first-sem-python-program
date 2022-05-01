a6443cf2f0b92929d26711e2d0b7ba9dfbf7f0d3
shiftbackspace <sah86148@gmail.com>
2022-05-01 22:47:06 +0545

    python program updated

--- today10.py
+++ today10.py
@@ -1,7 +1,7 @@
 1#. Print "1" if a is equal to b, print "2" if a is greater than b, otherwise print "3".
 # Python program showing
 # a use of input()
-print("Enter your value: ")
+print("Enter your value : ")
 a = input()
 print("Enter your value: ")
 b = input()
